15ee3ebce615505a939b2d41db9b60af084876d7
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-25 01:36:14 +0600

    set time out

---
+++ setTimeout.js
@@ -0,0 +1,10 @@
+/* function hello(){
+    console.log('third');
+} */
+console.log('first');
+console.log('second');
+setTimeout(() => {
+    console.log('third');
+}, 3000);
+console.log('fourth');
+console.log('fifth');
