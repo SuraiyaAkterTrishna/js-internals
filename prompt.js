6c0e9dddf85f025a846a63cdb7a4b4e0767182cb
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-27 17:06:42 +0600

    alert()

--- prompt.js
+++ prompt.js
@@ -2,3 +2,11 @@
 const showAlert = () => {
     alert("ki khobor kamon acho?");
 }; 
+
+const showConfirm = () => {
+    const number = Math.random()*10; 
+    console.log(number);
+    if(number < 5){
+        alert('Ki khobor dosto');
+    }
+}
